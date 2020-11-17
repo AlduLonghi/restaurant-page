482f7a35712647ef8e960d255df89e9dd6a58f28
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-11-17 06:11:41 -0300

    Complete contact section

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,32 @@
 const contactContainer = document.createElement('div');
 contactContainer.setAttribute('id', 'contact-btn-container');
-contactContainer.classList.add('tabbed-container', 'w-100', 'h-100');
-contactContainer.textContent += 'Hello contact';
+contactContainer.classList.add('tabbed-container', 'w-75', 'h-75', 'mx-auto', 'text-center', 'pt-5');
 contactContainer.style.display = 'none';
+
+const phoneTitle = document.createElement('h2');
+phoneTitle.textContent += 'Phone number';
+phoneTitle.classList.add('contact-title');
+
+const phoneNumber = document.createElement('p');
+phoneNumber.textContent += '+5648800334';
+phoneNumber.classList.add('contact-description');
+
+const adress = document.createElement('h2');
+adress.textContent += 'Adress';
+adress.classList.add('contact-title');
+
+const adressDescription = document.createElement('p');
+adressDescription.textContent += 'Lorem ipsum 8876, New York.';
+adressDescription.classList.add('contact-description');
+
+const reservations = document.createElement('h2');
+reservations.textContent += 'Reservations';
+reservations.classList.add('contact-title');
+
+const reservEmail = document.createElement('p');
+reservEmail.textContent += 'cheesemasters@gmail.com';
+reservEmail.classList.add('contact-description');
+
+contactContainer.append(phoneTitle, phoneNumber, adress, adressDescription, reservations, reservEmail);
 
 export default contactContainer;
